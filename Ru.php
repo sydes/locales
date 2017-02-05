01a1c0d4756e847fb1dbab61fc98b682d1ab2476
Arthur Grand <artygrand@gmail.com>
2017-02-05 22:12:56 +0700

    Added date translator

--- Ru.php
+++ Ru.php
@@ -13,4 +13,36 @@ class Ru extends Locale
     private $englishName = 'Russian';
     private $nativeName = 'Русский';
     private $isRtl = false;
+    
+    public function date()
+    {
+        $translate = [
+            'am' => 'дп', 'pm' => 'пп', 'AM' => 'ДП', 'PM' => 'ПП',
+            'Monday'    => 'Понедельник', 'Mon' => 'Пн',
+            'Tuesday'   => 'Вторник',     'Tue' => 'Вт',
+            'Wednesday' => 'Среда',       'Wed' => 'Ср',
+            'Thursday'  => 'Четверг',     'Thu' => 'Чт',
+            'Friday'    => 'Пятница',     'Fri' => 'Пт',
+            'Saturday'  => 'Суббота',     'Sat' => 'Сб',
+            'Sunday'    => 'Воскресенье', 'Sun' => 'Вс',
+            'Jan' => 'Янв', 'Feb' => 'Фев', 'Mar' => 'Мар', 'Apr' => 'Апр', 'Jun' => 'Июн',
+            'Jul' => 'Июл', 'Aug' => 'Авг', 'Sep' => 'Сен', 'Oct' => 'Окт', 'Nov' => 'Ноя', 'Dec' => 'Дек',
+            'st' => 'ое', 'nd' => 'ое', 'rd' => 'е', 'th' => 'ое',
+        ];
+
+        $months = strpos($format, 'F') === 0 ? [
+            'January'   => 'Январь',   'February' => 'Февраль', 'March'    => 'Март',   'April'    => 'Апрель',
+            'May'       => 'Май',      'June'     => 'Июнь',    'July'     => 'Июль',   'August'   => 'Август',
+            'September' => 'Сентябрь', 'October'  => 'Октябрь', 'November' => 'Ноябрь', 'December' => 'Декабрь',
+        ] : [
+            'January'   => 'Января',   'February' => 'Февраля', 'March'    => 'Марта',  'April'    => 'Апреля',
+            'May'       => 'Мая',      'June'     => 'Июня',    'July'     => 'Июля',   'August'   => 'Августа',
+            'September' => 'Сентября', 'October'  => 'Октября', 'November' => 'Ноября', 'December' => 'Декабря',
+        ];
+
+        $date = func_num_args() == 2 ? date($format, func_get_arg(1)) : date($format);
+        $translate = array_merge($translate, $months);
+
+        return strtr($date, $translate);
+    }
 }
